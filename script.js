c0994a210f611b4f2b9b25e49d69507b487ad082
majestice_coder <142192726+Kapil9711@users.noreply.github.com>
2024-05-05 11:22:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const mapLetters = (str) =>{
+	const obj = {};
+	str.trim().split('').forEach((letter,i)=> obj[letter] ? obj[letter].push(i) : obj[letter] =  [i]);
+	return obj;
+}
+
+
+
+
+
